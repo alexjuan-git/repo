31fd64a83f409efe72e7d55ef0697e696ff528e9
alexjuan-git <ajuanh@esliceu.net>
2026-04-27 13:14:30 +0200

    Run operacions.sh

---
+++ operacions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#variables són "paraules" que contenen un valor
+NOM="Miquel"
+#read serveix per llegir la informació del teclat 
+read -p "intrdueix un nom: " NOM
+
+echo "El teu nom es ${NOM}"
