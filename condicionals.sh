39a8b6a4f49010cc8c92e23ac232b0a91fef52f3
alexjuan-git <ajuanh@esliceu.net>
2026-04-27 13:37:33 +0200

    Introdueix condicionals.sh

---
+++ condicionals.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -p "Introdueixi X: " X
+
+if [[ ${X} -eq 5 ]]
+then
+    echo "has introduit un 5"
+    echo "enorabona"
+else
+    echo "no has introduit un 5"
+    echo "aixi no és"
+fi
