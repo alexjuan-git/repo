a70064ef864ae67682103aeb79764427f2068c53
alexjuan-git <ajuanh@esliceu.net>
2026-04-30 12:49:28 +0200

    Crear crear_user.sh

---
+++ crear_user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function usage(){
+    echo "Usage ${0}"
+    echo
+    echo "has de ser root"
+    echo "l'usuari no és pot repetir"
+    exit 1
+}
+
+#script per crear usuari
+
+#primer comprovam si som root
+if [[ ${UID} -ne 0]]
+then
+    usage
+fi
+
+read -p "Introdueix el nom d'usuari: " USER_NAME
